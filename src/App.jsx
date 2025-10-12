44e46a7c9806ce6842ae232b262d6e7640e1fe47
PoornachanderReddy <poornachander295@gmail.com>
2025-10-12 19:45:00 +0530

    course component and seo and emailjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MernCourse from "./components/MernCourse";
+import Courses from "./components/Courses";
+import { sendEmail } from "./nodeMailerServer";
 
 const App = () => {
+  useEffect(()=>{
+    // sendEmail();
+  },[])
   return (
     <div>
       <MernCourse />
-    </div>
+      <Courses/>
+    </div> 
   );
 };
 
